03e351bacb02dfc710c632c246e653f263d5b526
HermonaDev <hermonaaddisu@gmail.com>
2023-10-10 20:55:48 +0300

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "hasset-backend.vercel.app/api/";
+const BASE_URL = "hasset-backend.vercel.app/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZjQ4MTFmNTcwNjg4YTIxZDljOWU5OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NDI5MzQ4MCwiZXhwIjoxNjk0NzI1NDgwfQ.VeYzH4LEJ7CsXak0JoURSNT4MAyoz-A7ZN_Afjh9k8c"
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
